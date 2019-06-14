376d5a1d6b23991394e03a2b267c7a54f9e59f85
Ronnapob Ritprasert <ronnyrit@gmail.com>
2019-06-13 21:26:36 -0700

    Added log, Moved JS from tutorial.html to tutorial.js

---
+++ tutorial.js
@@ -0,0 +1,77 @@
+const ANT = "ye, i see, dang.. a monster...";
+var helloButton = document.getElementById("hello");
+var ageButton = document.getElementById("age");
+var dangButton = document.getElementById("dang");
+var ageh = document.getElementById("ageh");
+var spam = document.getElementById("spam");
+var legal = document.getElementById("legal");
+
+const obj = {
+    name: "Gorilla", // key - name, value = "Gorilla"
+    age: 13          // key = age, value = 13
+}; // Object with properties
+// To delete a property do,
+// delete obj.age; //Now the property is deleted.
+
+const array = [1,2,3,4,5];
+const array2 = [1,2,null,4,'five'];
+const array3 = ['hello','world','array'];
+const array4 = [
+    [1,2,3],
+    [4,5,6]
+];
+
+helloButton.addEventListener("click", function() {
+    console.log("Hello.. whirld");
+});
+ageButton.addEventListener("click", function() {
+    spam.innerHTML += "<br>" + 22;
+});
+dangButton.addEventListener("click", function() {
+    spam.innerHTML += "<br>" + ANT;
+});
+legal.addEventListener("click", function() {
+    document.getElementById("nope").removeAttribute("hidden");
+});
+
+console.log(array4);
+array4[0] = "hi";
+console.log(array4);
+
+// Shows current date.
+const currentDate = new Date();
+console.log(currentDate);
+
+//create date with time
+const date = new Date(2018, 9, 13, 15, 0);
+console.log(date);
+
+//Create Map
+const roles = new Map();
+
+//assign role to users
+//roles.set(t1, 'User1');
+//roles.set(t2, 'User2');
+
+//Create set (dublicates are not allowed)
+const roles2 = new Set();
+roles2.add('User1');
+
+//What is escaping? oh it's just doing \n \' \" so it doesn't get error.
+let str = ' HUH, "hello" ';
+let str2 = "hi";
+
+//cool backtick trick;
+let str3 = `Line 1
+Line 2
+            using backtick`;
+console.log(str3);
+let what = 5 * '5';
+let aaa = 5 + '5';
+console.log(what + " " + aaa);
+
+//for in loop
+// only works for object data types
+for (let o in obj) {
+    console.log(obj[o]);
+}
