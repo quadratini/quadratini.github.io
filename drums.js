2bf27f50310274e15ad43a1fee5bf4ad9d460f32
Ronnapob Ritprasert <ronnyrit@gmail.com>
2019-06-28 15:30:09 -0700

    Changed button positions

--- drums.js
+++ drums.js
@@ -20,24 +20,21 @@ var sounds = [];
             src: ['audio/cymbal2.wav']
         });
         var sound6 = new Howl({
-            src: ['audio/cymbal3.wav']
-        });
-        var sound7 = new Howl({
             src: ['audio/tom1.wav']
         });
-        var sound8 = new Howl({
+        var sound7 = new Howl({
             src: ['audio/realtom1.wav']
         });
-        var sound9 = new Howl({
+        var sound8 = new Howl({
             src: ['audio/realtom2.wav']
         });
-        var sound10 = new Howl({
+        var sound9 = new Howl({
             src: ['audio/realtom3.wav']
         });
-        var sound11 = new Howl({
+        var sound10 = new Howl({
             src: ['audio/realtom4.wav']
         });
-        var sound12 = new Howl({
+        var sound11 = new Howl({
             src: ['audio/rim.wav']
         });
 
@@ -46,8 +43,7 @@ function createfunc(i) {
 
         var button = document.getElementById("bd" + i);
         sounds = [sound0, sound1, sound2, sound3, sound4, sound5, sound6,
-                        sound7, sound8, sound9, sound10, sound11, sound12,
-                        ];
+                        sound7, sound8, sound9, sound10, sound11];
 
         button.addEventListener("click", () => {
             sounds[i].play(); 
@@ -55,28 +51,18 @@ function createfunc(i) {
     }
 }
 
-for (var i = 0; i < 13; i++) {
+for (var i = 0; i < 12; i++) {
     funcs[i] = createfunc(i);
 }
 
-for (var j = 0; j < 13; j++) {
+for (var j = 0; j < 12; j++) {
     funcs[j]();
 }
 
 onkeyup = function (e) {
-    document.getElementById('bd0').classList.remove("button-active");
-    document.getElementById('bd1').classList.remove("button-active");
-    document.getElementById('bd2').classList.remove("button-active");
-    document.getElementById('bd3').classList.remove("button-active");
-    document.getElementById('bd4').classList.remove("button-active");
-    document.getElementById('bd5').classList.remove("button-active");
-    document.getElementById('bd6').classList.remove("button-active");
-    document.getElementById('bd7').classList.remove("button-active");
-    document.getElementById('bd8').classList.remove("button-active");
-    document.getElementById('bd9').classList.remove("button-active");
-    document.getElementById('bd10').classList.remove("button-active");
-    document.getElementById('bd11').classList.remove("button-active");
-    document.getElementById('bd12').classList.remove("button-active");
+    for (var i = 0; i < 12; i++) {
+        document.getElementById('bd' + i).classList.remove("button-active");
+    }
 }
 
 
@@ -103,45 +89,42 @@ window.addEventListener("keydown", () => {
             sounds[2].play();
             buttons[2].classList.add("button-active");
             break;
-        case 82:
+        case 73:
             sounds[3].play();
             buttons[3].classList.add("button-active");
             break;
-        case 84:
+        case 79:
             sounds[4].play();
             buttons[4].classList.add("button-active");
             break;
-        case 89:
+        case 80:
             sounds[5].play();
             buttons[5].classList.add("button-active");
             break;
-        case 85:
+        case 65:
             sounds[6].play();
             buttons[6].classList.add("button-active");
             break;
-        case 73:
+        case 83:
             sounds[7].play();
             buttons[7].classList.add("button-active");
             break;
-        case 65:
+        case 68:
             sounds[8].play();
             buttons[8].classList.add("button-active");
             break;
-        case 83:
+        case 74:
             sounds[9].play();
             buttons[9].classList.add("button-active");
             break;
-        case 68:
+        case 75:
             buttons[10].classList.add("button-active");
             sounds[10].play();
             break;
-        case 70:
+        case 76:
             sounds[11].play();
             buttons[11].classList.add("button-active");
             break;
-        case 71:
-            sounds[12].play();
-            buttons[12].classList.add("button-active");
     }
 });
 buttons[0].addEventListener("keyup", function (e) {
